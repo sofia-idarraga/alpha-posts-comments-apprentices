78fdf8f903924ef5d1fb022afdb13bf828577737
santiagoposadag <santiago90dell@gmail.com>
2022-08-29 11:11:39 -0500

    base project ready

---
+++ src/main/java/com/sofka/alphapostcomments/AlphaPostCommentsApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.alphapostcomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaPostCommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaPostCommentsApplication.class, args);
+	}
+
+}
